f7f4a68ded1efa93eafc3e03da8c0c98c38f814c
profe-ssor <kyerematengcollins93@gmail.com>
2025-11-07 14:21:16 +0000

    Add App component and fix imports in main.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Osko Buys</h1>
+      {/* Add your app content here */}
+    </div>
+  );
+}
+
+export default App;
